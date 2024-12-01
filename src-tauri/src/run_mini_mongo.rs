c5cefa4556f66310901ad9a3624f8e5e09af0e80
wsds <wsds888@163.com>
2024-12-01 19:39:05 +0800

    to 0.5.4

--- src-tauri/src/run_mini_mongo.rs
+++ src-tauri/src/run_mini_mongo.rs
@@ -1,9 +1,3 @@
-use std::thread;
-
-pub fn start_mini_mongo_server_sub_thread(){
-    thread::spawn(|| {
-        println!("start mini_mongo_server in sub_thread. 1002");
-        let _ = minimongo::start_mmg_server();
-        println!("start mini_mongo_server finished. 1002");
-    });
+pub fn start_mini_mongo_server_sub_thread() {
+    let _ = minimongo::start_mmg_server_sub_thread();
 }
